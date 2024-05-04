07b48390ac7993874cfcc74f0d5cd33c1f98bbdc
NataliLebedieva <4155672@gmail.com>
2024-05-04 22:56:32 +0300

    com3

--- m3_hw1.py
+++ m3_hw1.py
@@ -1,20 +1,17 @@
 from datetime import datetime, timedelta
 
 def get_days_from_today(date):
-    try:
-
-    
+    try:    
         now = datetime.today()
         now_date = now.date()
         print(f'Поточна дата: {now_date}')
-
+        date_d = datetime.strptime(date, '%Y-%m-%d').date()
         difference = now_date-date_d
         print(f'Різниця між поточною датою та заданою датою: {difference.days}')
     except ValueError:
         print("Неправильний формат дати. Введіть дату у форматі 'РРРР-ММ-ДД'.")
 
 date=input('Задайте дату в форматі РРРР-ММ-ДД: ')
-date_d = datetime.strptime(date, '%Y-%m-%d').date()
 get_days_from_today(date)
 
 
